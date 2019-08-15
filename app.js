6d8296ca2ccaef527bc624f6e569b73d4c5f4a5b
lprice93 <lprice@wilbron.com>
2019-08-14 20:57:26 -0500

    lab to prompt 4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let btn = document.createElement('button');
     let btnText = document.createTextNode('Click Me');
     btn.append(btnText);
-    document.body.append(btn);
+    document.body.prepend(btn);
     btn.style.display = 'block';
 
     btn.addEventListener('click', function () {
@@ -11,11 +11,39 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    let btn2 = document.getElementsByClassName('button-2');
-    btn2[0].addEventListener('click', function () {
+    let btn2 = document.getElementsByClassName('button-2')[0];
+    btn2.addEventListener('click', function () {
         let text = document.getElementsByClassName('input')[0].value;
         if (text !== 0) {
             alert(text);
         };
     });
+
+    let box = document.getElementsByClassName('div')[0];
+    box.addEventListener("mouseover", function () {
+        box.style.backgroundColor = 'orange';
+    });
+
+    box.addEventListener("mouseleave", function () {
+        box.style.backgroundColor = '';
+    });
+
+    let para = document.createElement("p");
+    let copy = document.createTextNode("Hey, I am some random text!");
+    para.append(copy);
+    document.body.appendChild(para);
+
+    para.addEventListener("click", function () {
+        function getRandomColor() {
+            let colors = ['orange', 'lightblue', 'lightgreen', 'salmon', 'olive', 'darkcyan', 'lightpurple', 'maroon'];
+            let randomIndex = Math.floor(Math.random() * colors.length);
+            return colors[randomIndex];
+        } para.style.color = getRandomColor();
+    });
+
+    // let btn3 = document.createElement('button-3');
+    // let btn3Text = document.createTextNode('Do Something');
+    // btn3.append(btn3Text);
+    // document.body.prepend(btn3);
+
 });
